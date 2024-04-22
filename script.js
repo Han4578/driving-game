8fc83bcc6efe037cbb3d5e923e8deddbe9218b5e
Han4578 <99018603+Han4578@users.noreply.github.com>
2024-04-22 19:22:10 +0800

    added scaling and basic mobile control

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 let canvas = document.querySelector("canvas")
 let ctx = canvas.getContext("2d")
-let width = innerWidth
-let height = width / 16 * 9
-let speed = 10
-let playerSpeed = 5
+let width = window.innerWidth
+let height = canvas.getBoundingClientRect().height
+let speed = scaleWidth(1)
+let playerSpeed = scaleHeight(0.5)
 let colours = ["orange", "green", "darkblue", "purple", "yellow", "white", "orange"]
 let cars = []
 let bars = []
 let moveUp = false
 let moveDown = false
 let continueGame = true
+let carWidth = 140
+let deltaTime = Date.now()
+let resumeButton = document.querySelector(".resume")
+let carDelay = deltaTime
+let barDelay = deltaTime
+let pauseTime = 0
 
 canvas.width = width
 canvas.height = height
 
 player = {
-    x: 100,
-    y: 100,
+    x: scaleWidth(100),
+    y: height / 2 - scaleHeight(40),
     color: "red",
-    width: 100,
-    height: 50
+    width: scaleWidth(carWidth),
+    height: scaleHeight(80)
 }
 
 document.addEventListener("keydown", (e) => {
@@ -32,37 +38,82 @@ document.addEventListener("keyup", (e) => {
     if (["ArrowDown", "s", 'S'].includes(e.key)) moveDown = false
 })
 
+document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState != "visible") pause()
+})
+
+canvas.addEventListener("pointerdown", e => {
+    if (e.clientX <= width / 2) moveUp = true
+    else moveDown = true
+})
+
+canvas.addEventListener("pointerup", e => {
+    if (e.clientX <= width / 2) moveUp = false
+    else moveDown = false
+})
+
+canvas.addEventListener("pointercancel", () => {
+    moveUp = moveDown = false
+})
+
+
+resumeButton.addEventListener("click", resume)
+
+for (let i = 0; i < 5; i++) {
+    bars.push({
+        x: width - 500 * i * speed * 1.2,
+        time: Date.now()
+    })
+    
+}
+
 function nextFrame() {
     if (!continueGame) return
-    if (moveUp) player.y -= playerSpeed
-    if (moveDown) player.y += playerSpeed
+    let currentTime = Date.now()
+
+    if (moveUp) player.y -= playerSpeed * (currentTime - deltaTime)
+    if (moveDown) player.y += playerSpeed * (currentTime - deltaTime)
+    if (player.y < 0) player.y = 0
+    if (player.y + player.height > height) player.y = height - player.height
+
     ctx.clearRect(0, 0, width, height)
+
     for (let bar of bars) {
-        bar.x -= speed
+        bar.x -= (currentTime - deltaTime) * speed * 1.2
         drawBars(bar.x)
     }
     drawCar(player)
     for (const car of cars) {
-        car.x -= speed
+        car.x -= (currentTime - deltaTime) * speed
         drawCar(car)
     }
     cars = cars.filter(c => {return c.x + c.width > 0})
-    bars = bars.filter(c => {return c.x + 60> 0})
+    bars = bars.filter(c => {return c.x + scaleWidth(60)> 0})
+    if (currentTime - carDelay >= 1000) {
+        addCar()
+        carDelay = currentTime
+    }
+    if (currentTime - barDelay >= 500) {
+        addBar()
+        barDelay = currentTime
+    }
+    deltaTime = currentTime
     if (cars.some(c => {return checkCollison(c, player)})) endGame()
     window.requestAnimationFrame(nextFrame)
 }
 
 function drawCar(car) {
+    let headlights = 30
     ctx.fillStyle = car.color
     ctx.fillRect(car.x, car.y, car.width, car.height)
     ctx.fillStyle = "lightblue"
-    ctx.fillRect(car.x + car.width - 20, car.y + car.height - 20, 20, 20)
-    ctx.fillRect(car.x + car.width - 20, car.y, 20, 20)
+    ctx.fillRect(car.x + car.width - scaleWidth(headlights), car.y + car.height - scaleHeight(headlights), scaleWidth(headlights), scaleHeight(headlights))
+    ctx.fillRect(car.x + car.width - scaleWidth(headlights), car.y, scaleWidth(headlights), scaleHeight(headlights))
 }
 
 function drawBars(x) {
     ctx.fillStyle = "white"
-    ctx.fillRect(x, height / 2 - 10, 60, 20)
+    ctx.fillRect(x, height / 2 - 10, scaleWidth(60), scaleHeight(20))
 }
 
 function checkCollison(c, player) {
@@ -74,20 +125,47 @@ function endGame() {
     continueGame = false
 }
 
-setInterval(() => {
-    let w = Math.random() * 100 + 100
+function scaleWidth(val) {
+    return val / 1024 * width
+}
+
+function scaleHeight(val) {
+    return val / 576 * height
+}
+
+function pause() {
+    pauseTime = Date.now()
+    continueGame = false
+    resumeButton.classList.add("display")
+}
+
+function resume() {
+    carDelay += Date.now() - pauseTime
+    barDelay += Date.now() - pauseTime
+    continueGame = true
+    resumeButton.classList.remove("display")
+    deltaTime = Date.now()
+    window.requestAnimationFrame(nextFrame)
+}
+
+function addCar() {
+    let w = Math.random() * carWidth + carWidth
     let car = {
-        width: w,
-        height: 50,
+        width: scaleWidth(w),
+        height: scaleHeight(80),
         x: width + w,
-        y: Math.random() * (height - 50),
-        color: colours[Math.round(Math.random() * (colours.length - 1))]
+        y: Math.random() * (height - scaleHeight(80)),
+        color: colours[Math.round(Math.random() * (colours.length - 1))],
+        time:  Date.now()
     }
     cars.push(car)
-}, 1000)
+}
 
-setInterval(() => {
-    bars.push({x: width})
-}, 500)
+function addBar() {
+    bars.push({
+        x: width,
+        time: Date.now()
+    })
+}
 
 window.requestAnimationFrame(nextFrame)
